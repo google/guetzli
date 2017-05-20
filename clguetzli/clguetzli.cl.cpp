197fd33b870f00116807d098b8c09b654efb94ee
strongtu <tuq@c#1507>
2017-05-20 22:10:04 +0800

    精简代码

---
+++ clguetzli/clguetzli.cl.cpp
@@ -0,0 +1,145 @@
+#include <algorithm>
+#include <stdint.h>
+#include "utils.h"
+#include "clguetzli_comparator.h"
+
+extern bool g_useOpenCL;
+extern bool g_checkOpenCL;
+
+using namespace std;
+
+int get_global_id(int dim)
+{
+    return 0;
+}
+
+int get_global_size(int dim)
+{
+    return 0;
+}
+
+#define abs(exper)    fabs((exper))
+
+#define __opencl
+#include "clguetzli.cl"
+
+
+namespace guetzli
+{
+    ButteraugliComparatorEx::ButteraugliComparatorEx(const int width, const int height,
+        const std::vector<uint8_t>* rgb,
+        const float target_distance, ProcessStats* stats)
+        : ButteraugliComparator(width, height, rgb, target_distance, stats)
+    {
+
+    }
+
+    void ButteraugliComparatorEx::StartBlockComparisons()
+    {
+        ButteraugliComparator::StartBlockComparisons();
+
+        const int width = width_;
+        const int height = height_;
+        const int factor_x = 1;
+        const int factor_y = 1;
+
+        const int block_width = (width + 8 * factor_x - 1) / (8 * factor_x);
+        const int block_height = (height + 8 * factor_y - 1) / (8 * factor_y);
+        const int num_blocks = block_width * block_height;
+
+        const double* lut = kSrgb8ToLinearTable;
+
+        imgOpsinDynamicsBlockList.resize(num_blocks * 3 * kDCTBlockSize);
+        imgMaskXyzScaleBlockList.resize(num_blocks * 3);
+        for (int block_y = 0, block_ix = 0; block_y < block_height; ++block_y)
+        {
+            for (int block_x = 0; block_x < block_width; ++block_x, ++block_ix)
+            {
+                float* curR = &imgOpsinDynamicsBlockList[block_ix * 3 * kDCTBlockSize];
+                float* curG = curR + kDCTBlockSize;
+                float* curB = curG + kDCTBlockSize;
+
+                for (int iy = 0, i = 0; iy < 8; ++iy) {
+                    for (int ix = 0; ix < 8; ++ix, ++i) {
+                        int x = std::min(8 * block_x + ix, width - 1);
+                        int y = std::min(8 * block_y + iy, height - 1);
+                        int px = y * width + x;
+
+                        curR[i] = lut[rgb_orig_[3 * px]];
+                        curG[i] = lut[rgb_orig_[3 * px + 1]];
+                        curB[i] = lut[rgb_orig_[3 * px + 2]];
+                    }
+                }
+
+                int xmin = block_x * 8;
+                int ymin = block_y * 8;
+
+                imgMaskXyzScaleBlockList[block_ix * 3] = mask_xyz_[0][ymin * width_ + xmin];
+                imgMaskXyzScaleBlockList[block_ix * 3 + 1] = mask_xyz_[1][ymin * width_ + xmin];
+                imgMaskXyzScaleBlockList[block_ix * 3 + 2] = mask_xyz_[2][ymin * width_ + xmin];
+            }
+        }
+    }
+
+    void ButteraugliComparatorEx::FinishBlockComparisons() {
+        ButteraugliComparator::FinishBlockComparisons();
+
+        imgOpsinDynamicsBlockList.clear();
+        imgMaskXyzScaleBlockList.clear();
+    }
+
+    void ButteraugliComparatorEx::SwitchBlock(int block_x, int block_y, int factor_x, int factor_y)
+    {
+        block_x_ = block_x;
+        block_y_ = block_y;
+        factor_x_ = factor_x;
+        factor_y_ = factor_y;
+
+        ButteraugliComparator::SwitchBlock(block_x, block_y, factor_x, factor_y);
+    }
+
+    double ButteraugliComparatorEx::CompareBlock(const OutputImage& img, int off_x, int off_y, const coeff_t* candidate_block, const int comp_mask) const
+    {
+        double err = ButteraugliComparator::CompareBlock(img, off_x, off_y, candidate_block, comp_mask);
+        return err;
+        if (g_checkOpenCL)
+        {
+            double err1 = ButteraugliComparator::CompareBlock(img, off_x, off_y, candidate_block, comp_mask);
+            if (err1 != err)
+            {
+                LogError("CHK %s(%d) \r\n", __FUNCTION__, __LINE__);
+            }
+        }
+
+        return err;
+    }
+
+    double ButteraugliComparatorEx::CompareBlockEx(const OutputImage& img, int off_x, int off_y, const coeff_t* candidate_block, const int comp_mask) const
+    {
+        const int block_x = block_x_;
+        const int block_y = block_y_;
+        const int factor = factor_x_;
+
+        const coeff_t *candidate_channel[3];
+        channel_info mayout_channel[3];
+        for (int c = 0; c < 3; c++)
+        {
+            candidate_channel[c] = &candidate_block[c * 8 * 8];
+            mayout_channel[c].block_height = img.component(c).height_in_blocks();
+            mayout_channel[c].block_width = img.component(c).width_in_blocks();
+            mayout_channel[c].factor = img.component(c).factor_x();
+            mayout_channel[c].pixel = img.component(c).pixels();
+            mayout_channel[c].coeff = img.component(c).coeffs();
+        }
+
+        return CompareBlockFactor(mayout_channel,
+            candidate_block,
+            block_x,
+            block_y,
+            imgOpsinDynamicsBlockList.data(),
+            imgMaskXyzScaleBlockList.data(),
+            width_,
+            height_,
+            factor_x_);
+    }
+}
