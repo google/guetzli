0c85b8fe63ce86e1935f936bcff06bf6f9b1febd
strongtu <tuqbasic@qq.com>
2017-06-07 18:50:10 +0800

    换一组编译参数

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@
 echo $1 --machine 64 or 32
 echo $2 -G
 
-nvcc -I"./" -I"/usr/local/cuda/include" -arch=compute_30 --machine $1 $2 -ptx -o clguetzli/clguetzli.cu.ptx$1  clguetzli/clguetzli.cu
+nvcc -I"./" -I"/usr/local/cuda/include" -use-fast_math -ftz=true -prec-div=false -prec-sqrt=false -arch=compute_30 -O3 --machine $1 $2 -ptx -o clguetzli/clguetzli.cu.ptx$1  clguetzli/clguetzli.cu
 
 #copy to ./bin/Release
 cp clguetzli/clguetzli.cu.ptx$1 bin/Release/clguetzli/clguetzli.cu.ptx$1
