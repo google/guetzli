52bce1be7d74661f27054d57cbccde98f04c27db
strongtu <tuqbasic@qq.com>
2017-06-07 12:32:04 +0800

    fix test batch

--- tests/banner/test.bat
+++ tests/banner/test.bat
@@ -2,20 +2,20 @@
 echo CPU Time
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
 echo %CURRENT_TIME%
-guetzli.exe big.jpg big.cpu.out.jpg
+guetzli.exe big.jpg big.cout.jpg
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
 echo %CURRENT_TIME%
 
 echo OpenCL Time
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
 echo %CURRENT_TIME%
-guetzli.exe --opencl big.jpg big.gpu.out.jpg
+guetzli.exe --opencl big.jpg big.opencl.jpg
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
 echo %CURRENT_TIME%
 
 echo CUDA Time
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
 echo %CURRENT_TIME%
-guetzli.exe --cuda big.jpg big.gpu.out.jpg
+guetzli.exe --cuda big.jpg big.cuda.jpg
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
 echo %CURRENT_TIME%
