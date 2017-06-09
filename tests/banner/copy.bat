1cc2de4d7e3339cc78b11788808f9fa977929b58
strongtu <tuq@c#1507>
2017-06-10 00:08:27 +0800

    清理无用脚本

--- tests/banner/copy.bat
+++
@@ -1,6 +0,0 @@
-mkdir clguetzli
-echo f|xcopy ..\..\clguetzli\clguetzli.cl clguetzli\clguetzli.cl /y
-echo f|xcopy ..\..\clguetzli\clguetzli.cl.h clguetzli\clguetzli.cl.h /y
-echo f|xcopy ..\..\clguetzli\clguetzli.cu.ptx64 clguetzli\clguetzli.cu.ptx64 /y
-
-echo f|xcopy ..\..\bin\x86_64\Release\guetzli.exe guetzli.exe /y
