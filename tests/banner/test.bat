78a14550bab351ee0afad685461c989a980e0f06
strongtu <tuq@c#1507>
2017-06-09 11:29:46 +0800

    调整测试脚本

--- tests/banner/test.bat
+++ tests/banner/test.bat
@@ -1,17 +1,17 @@
 @echo off
+call copy.bat
+
 set IMG_DIR=.\imgs
 set exe="guetzli.exe"
 
 @echo on
 
-echo CUDA Time
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
-echo %CURRENT_TIME%
+echo CUDA Time Start = %CURRENT_TIME%
 
 for /f "delims=\" %%i in ('dir /b /a-d /o-d "%IMG_DIR%\*.jpg" ') do (
 %exe% --opencl %IMG_DIR%\%%i  .\out\%%i
 )
 
-echo CUDA Time
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
-echo %CURRENT_TIME%
+echo CUDA Time End = %CURRENT_TIME%
