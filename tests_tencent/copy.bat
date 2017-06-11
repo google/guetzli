41e008cde439d24c96e6650118f722509a086338
strongtu <tuq@c#1507>
2017-06-12 03:05:26 +0800

    增加批量处理脚本

--- tests_tencent/copy.bat
+++ tests_tencent/copy.bat
@@ -1,6 +1,6 @@
 mkdir clguetzli
-echo f|xcopy ..\clguetzli\clguetzli.cl clguetzli\clguetzli.cl /y
-echo f|xcopy ..\clguetzli\clguetzli.cl.h clguetzli\clguetzli.cl.h /y
-echo f|xcopy ..\clguetzli\clguetzli.cu.ptx64 clguetzli\clguetzli.cu.ptx64 /y
+echo f|copy ..\clguetzli\clguetzli.cl clguetzli\clguetzli.cl /y
+echo f|copy ..\clguetzli\clguetzli.cl.h clguetzli\clguetzli.cl.h /y
+echo f|copy ..\clguetzli\clguetzli.cu.ptx64 clguetzli\clguetzli.cu.ptx64 /y
 
-echo f|xcopy ..\bin\x86_64\Release\guetzli.exe guetzli.exe /y
+echo f|copy ..\bin\x86_64\Release\guetzli.exe guetzli.exe /y
--- tests_tencent/test.bat
+++ tests_tencent/test.bat
@@ -1,16 +1,27 @@
 @echo off
-call copy.bat
+@call copy.bat
 
-set IMG_DIR=.\imgs
-set exe="guetzli.exe"
+@set IMG_DIR=imgs_in
+@set IMG_OUTDIR=imgs_out
+@set IMG_EXT=*.jpg
 
-@echo on
+@set exe="guetzli.exe"
+
+setlocal enabledelayedexpansion
 
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
 echo CUDA Time Start = %CURRENT_TIME%
 
-for /f "delims=\" %%i in ('dir /b /a-d /o-d "%IMG_DIR%\*.jpg" ') do (
-%exe% --opencl %IMG_DIR%\%%i  .\out\%%i
+for /f "delims=" %%i in ('dir /b/a-d/s %IMG_DIR%\%IMG_EXT%') do (
+set fileName=%%~nxi
+set path=%%~dpi
+set dest_path=!path:%IMG_DIR%=%IMG_OUTDIR%!
+
+mkdir !dest_path! >nul 2>nul
+
+rem echo "!path!!fileName!"
+rem echo "!dest_path!!fileName!"
+%exe% --opencl "!path!!fileName!"  "!dest_path!!fileName!"
 )
 
 set CURRENT_TIME=%time:~0,2%:%time:~3,2%:%time:~6,2%
