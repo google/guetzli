e5f5f11a4f92770936db230eccd731f0acc80439
Zhou Ke <crazyks@yeah.net>
2017-07-11 11:10:52 +0800

    Remove some redundant files

--- tests_tencent/copy.bat
+++
@@ -1,6 +0,0 @@
-mkdir clguetzli
-echo f|copy ..\clguetzli\clguetzli.cl clguetzli\clguetzli.cl /y
-echo f|copy ..\clguetzli\clguetzli.cl.h clguetzli\clguetzli.cl.h /y
-echo f|copy ..\clguetzli\clguetzli.cu.ptx64 clguetzli\clguetzli.cu.ptx64 /y
-
-echo f|copy ..\bin\x86_64\Release\guetzli.exe guetzli.exe /y
--- tests_tencent/testLinux/cp2test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir clguetzli
-cp ../../bin/Release/guetzli guetzli
-cp ../../clguetzli/* clguetzli/
