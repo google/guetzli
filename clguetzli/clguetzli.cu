0a25dad419538ff206ffaa14baef2dfa8e607eb4
strongtu <tuqbasic@qq.com>
2017-07-24 09:31:31 +0800

    Add OpenCL Support

---
+++ clguetzli/clguetzli.cu
@@ -0,0 +1,22 @@
+#include "clguetzli/clguetzli.cl"
+/*
+__device__ int get_global_id(int dim)
+{
+    switch (dim)
+    {
+    case 0:
+        return threadIdx.x;
+    case 1:
+        return threadIdx.y;
+    case 2:
+        return threadIdx.z;
+    default:
+        return threadIdx.x;
+    }
+}
+
+__device__ int get_global_size(int dim)
+{
+    return 0;
+}
+*/
